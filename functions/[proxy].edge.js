55a76efb083367301eca82f87c1e65b0c8703465
sanikapareek-contentstack <122089051+sanikapareek-contentstack@users.noreply.github.com>
2024-05-13 19:25:49 +0530

    Update [proxy].edge.js

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,18 +1,20 @@
-import Personalization from '@contentstack/personalization-sdk-js';
+export default async function handler(req, context) {
+  const parsedUrl = new URL(req.url);
+  const route = parsedUrl.pathname;
 
-export async function handler(req, context) {
-  console.log("🚀 ~ handler ~ req:", req.url)
-  console.log(context.env)
-  console.log(context.waitUntil)
-  const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
-  let response = await res.json();
-  response = {
-    ...response,
-    time: new Date()
-  }
-  return new Response(JSON.stringify(response), {
-    headers: {
-      'X-Message': 'Change response headers'
+  if (route === '/test') {
+    const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
+    let response = await res.json();
+    response = {
+      ...response,
+      time: new Date()
     }
-  })
+    return new Response(JSON.stringify(response), {
+      headers: {
+        'X-Message': 'Change response headers'
+      }
+    })
+  }
+
+  return fetch(req)
 }
