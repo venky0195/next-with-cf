2148635790a2fd4f9b3b9ab565b3838749d5b9c6
sanikapareek-contentstack <122089051+sanikapareek-contentstack@users.noreply.github.com>
2024-05-15 15:25:01 +0530

    Pass environment variable in the edge function response

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,13 +1,15 @@
 export default async function handler(req, context) {
   const parsedUrl = new URL(req.url);
   const route = parsedUrl.pathname;
+  const envVariable = context.env.TEST_KEY;
 
   if (route === '/test') {
     const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
     let response = await res.json();
     response = {
       ...response,
-      time: new Date()
+      time: new Date(),
+      envVariableValue: envVariable,
     }
     return new Response(JSON.stringify(response), {
       headers: {
